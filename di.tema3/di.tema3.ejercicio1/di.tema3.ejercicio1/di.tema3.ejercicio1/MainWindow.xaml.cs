331bf0f7dd759de9ce495db9d128dc420583de25
maavcode <maavcode@hotmail.com>
2025-10-15 08:07:35 +0200

    actualizacion

---
+++ di.tema3/di.tema3.ejercicio1/di.tema3.ejercicio1/di.tema3.ejercicio1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MahApps.Metro.Controls;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace di.tema3.ejercicio1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
