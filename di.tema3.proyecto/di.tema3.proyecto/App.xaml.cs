86d77bc660fa044f34abd92a7df8a0af5aff917c
maavcode <mario.aguilar.dev@hotmail.com>
2025-11-03 14:26:47 +0100

    proceso

---
+++ di.tema3.proyecto/di.tema3.proyecto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace di.tema3.proyecto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
